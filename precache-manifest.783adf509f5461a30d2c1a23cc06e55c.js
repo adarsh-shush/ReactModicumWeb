d149cb50c3c1bed8e9de839f8a0d4772185127bc
adarsh mahendrakar <adarshmahendrakar66.com>
2021-03-04 20:34:05 +0530

    Updates

---
+++ precache-manifest.783adf509f5461a30d2c1a23cc06e55c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06445532b8ed6684a3e9347ffc267aac",
+    "url": "/ReactModicumWeb/index.html"
+  },
+  {
+    "revision": "b16a90e73ede83da2367",
+    "url": "/ReactModicumWeb/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "eb92c24db6b9af7b19cf",
+    "url": "/ReactModicumWeb/static/js/2.1156ce35.chunk.js"
+  },
+  {
+    "revision": "9106121d9ee5938d92b695826364e36d",
+    "url": "/ReactModicumWeb/static/js/2.1156ce35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16a90e73ede83da2367",
+    "url": "/ReactModicumWeb/static/js/main.4f6e6a52.chunk.js"
+  },
+  {
+    "revision": "f8480c58d8bd618392de",
+    "url": "/ReactModicumWeb/static/js/runtime-main.374fbe99.js"
+  },
+  {
+    "revision": "309c5f827be3520ab0bd7ab37d7286a5",
+    "url": "/ReactModicumWeb/static/media/app1.309c5f82.jpg"
+  },
+  {
+    "revision": "b04e64029dc10453bf017682ae24bea1",
+    "url": "/ReactModicumWeb/static/media/aws.b04e6402.png"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/ReactModicumWeb/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "cdc0d0e159dd1e20416c71a1a4db8120",
+    "url": "/ReactModicumWeb/static/media/bulb.cdc0d0e1.jpg"
+  },
+  {
+    "revision": "71ebe10f13935e03ba33c80cb0cfb568",
+    "url": "/ReactModicumWeb/static/media/flutterlogo.71ebe10f.png"
+  },
+  {
+    "revision": "ff059597dd4e09b5b1de85990334f5f1",
+    "url": "/ReactModicumWeb/static/media/flywheel.ff059597.png"
+  },
+  {
+    "revision": "394e2ad983cb3ba92f8dd37a983b901e",
+    "url": "/ReactModicumWeb/static/media/hosptl1.394e2ad9.png"
+  },
+  {
+    "revision": "1d4461895aa9460c9d50d172dd6b4a41",
+    "url": "/ReactModicumWeb/static/media/img2.1d446189.jpg"
+  },
+  {
+    "revision": "eb7bf3670217e0d272cb05ad31937e40",
+    "url": "/ReactModicumWeb/static/media/img4.eb7bf367.jpg"
+  },
+  {
+    "revision": "5804ee4aa5863ef9f59d160e2b169763",
+    "url": "/ReactModicumWeb/static/media/kvjsoft.5804ee4a.png"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/ReactModicumWeb/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "598529d1e38fd8d6de5c8ad7ccf7a805",
+    "url": "/ReactModicumWeb/static/media/logo.598529d1.png"
+  },
+  {
+    "revision": "dcc0c0575bfd85dba0c4c846fad0cc2d",
+    "url": "/ReactModicumWeb/static/media/logosymbolpng.dcc0c057.png"
+  },
+  {
+    "revision": "3e8365ae88561944f755d884afafeaab",
+    "url": "/ReactModicumWeb/static/media/mongodb.3e8365ae.png"
+  },
+  {
+    "revision": "efd22267efa1c9589fe5611454dfe821",
+    "url": "/ReactModicumWeb/static/media/mysql.efd22267.png"
+  },
+  {
+    "revision": "24dec5d3ef88fcfba2781a2a6ea7b214",
+    "url": "/ReactModicumWeb/static/media/nodejs.24dec5d3.jpg"
+  },
+  {
+    "revision": "a4e2c58ab4b4c89dacdbc429210bba70",
+    "url": "/ReactModicumWeb/static/media/pexels-thisisengineering-3861972.a4e2c58a.jpg"
+  },
+  {
+    "revision": "246d4e8986b130da83dcef9ccf6e33ba",
+    "url": "/ReactModicumWeb/static/media/pngimg5.246d4e89.png"
+  },
+  {
+    "revision": "050614b9019297d53cfce547a3d92d29",
+    "url": "/ReactModicumWeb/static/media/pngimg6.050614b9.png"
+  },
+  {
+    "revision": "ec0a20ca715fcb24f76302992cca4b31",
+    "url": "/ReactModicumWeb/static/media/pngimg7.ec0a20ca.png"
+  },
+  {
+    "revision": "74946ad6a26ac98b3566294ee9d9d65c",
+    "url": "/ReactModicumWeb/static/media/pngimg8.74946ad6.png"
+  },
+  {
+    "revision": "54a130f69efb4ed781935f1f8ce4fbf0",
+    "url": "/ReactModicumWeb/static/media/programming-concept-illustration-vector.54a130f6.jpg"
+  },
+  {
+    "revision": "20b0b974b239a5e6bf785c553677cc9d",
+    "url": "/ReactModicumWeb/static/media/react-logo.20b0b974.png"
+  },
+  {
+    "revision": "9cfbe6672d44634acff2d4cfa8030105",
+    "url": "/ReactModicumWeb/static/media/reactnativelogo.9cfbe667.png"
+  },
+  {
+    "revision": "48652a19fb1a69f7861cd0344a0e185f",
+    "url": "/ReactModicumWeb/static/media/request2.48652a19.jpg"
+  },
+  {
+    "revision": "1392212cd45c1c3b6bf780ab3a62eb48",
+    "url": "/ReactModicumWeb/static/media/service1.1392212c.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/ReactModicumWeb/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "0881daccb5178410b18a1a36984da095",
+    "url": "/ReactModicumWeb/static/media/spring-boot-logo.0881dacc.png"
+  },
+  {
+    "revision": "2ab0138d71e6d11458b326a1ed87b859",
+    "url": "/ReactModicumWeb/static/media/testimonials.2ab0138d.jpg"
+  }
+]);
