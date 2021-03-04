6dfa5fe88b7b2378704e31e9758df75a022ab7df
adarshmahendrakar <52000829+adarshmahendrakar@users.noreply.github.com>
2021-03-04 22:50:18 +0530

    Update index.js
    Index AboutUs commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
-import AboutUs from "views/AboutUs";
+import AboutUs from "views/Components/AboutUs";
 import WebDevelopment from "views/Components/WebDevService";
 import PortfolioPage from './views/Portfolio';
 import ContactusPage from "views/Contactus";
